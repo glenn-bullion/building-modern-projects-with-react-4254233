ddea8a7ec7de7e5f43adfa39a6d8c3aa771ad0d5
glenn-bullion <glenn@bullionnow.com.au>
2025-02-07 15:40:19 +1100

    To do list working with property fall through.

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const App = () => {
-  return <h1>This is a React Script!</h1>
-}
+// const App = () => {
+//   return <h1>This is a React Script!</h1>
+// }
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />)
+root.render(<App message="Hi there from me!" />)
