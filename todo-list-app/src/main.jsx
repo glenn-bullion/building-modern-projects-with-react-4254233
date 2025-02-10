0f3a75c4c1a50288576cb8ff3831a7828022dfdf
glenn-bullion <glenn@bullionnow.com.au>
2025-02-10 15:17:26 +1100

    Another done

--- todo-list-app/src/main.jsx
+++ todo-list-app/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { todosSlice } from './todosSlice';
 import App from './App.jsx'
 import './index.css'
 
+const store = configureStore({
+  reducer: {
+    todos: todosSlice.reducer,
+  },
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
